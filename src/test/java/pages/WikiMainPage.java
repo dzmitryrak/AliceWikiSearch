64b33acd30013eff7ba1b3a5a7c4cb3b388ad803
dzmitry.rak <dmitryrak11@gmail.com>
2023-04-19 20:15:38 +0300

    Added wait for option list

--- src/test/java/pages/WikiMainPage.java
+++ src/test/java/pages/WikiMainPage.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WikiMainPage extends BasePage {
 
-    @FindBy(id = "searchInput")
+    @FindBy(name = "search")
     private WebElement searchField;
 
     public WikiMainPage(WebDriver driver) {
@@ -16,6 +18,7 @@ public class WikiMainPage extends BasePage {
 
     public void searchByKeyword(String keyword){
         searchField.sendKeys(keyword);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[role=option]")));
         driver.findElement(By.cssSelector(".cdx-search-input__end-button")).click();
     }
 }
